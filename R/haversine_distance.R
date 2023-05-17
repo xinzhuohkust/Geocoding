1b33d48a6848ca328b1aeabc44408ecefa903e1c
Hsincho <99251348+xinzhuohkust@users.noreply.github.com>
2023-05-18 03:08:36 +0800

    Update haversine_distance.R

--- R/haversine_distance.R
+++ R/haversine_distance.R
@@ -1,4 +1,5 @@
 haversine_distance <- function(x, y) {
-  geosphere::distm(unlist(x), unlist(y), fun = geosphere::distHaversine) / 1000
+  distance <- geosphere::distm(unlist(x), unlist(y), fun = geosphere::distHaversine) / 1000
+  return(distance[1, 1])
 }
 
